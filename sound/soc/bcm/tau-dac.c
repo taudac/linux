dafa6616b04dfbfeaf2280e460d4097eb4619af9
Sergej Sawazki <sawazki@gmx.de>
2015-03-09 12:42:51 +0100

    taudac: add drvdata

--- sound/soc/bcm/tau-dac.c
+++ sound/soc/bcm/tau-dac.c
@@ -21,54 +21,42 @@
 #include <sound/soc.h>
 #include <sound/pcm_params.h>
 
-#include <linux/gpio.h>
+#include <linux/of_gpio.h>
 #include <linux/i2c.h>
 #include <linux/clk.h>
 
 #include "../codecs/wm8741.h"
 
-
-//#define TAU_DAC_GPIO_MCLK_ENABLE 27
-//#define TAU_DAC_GPIO_MCLK_SELECT 22
-
-/*
-static struct gpio tau_dac_gpios[] = {
-	{TAU_DAC_GPIO_MCLK_ENABLE, GPIOF_OUT_INIT_LOW, "TauDAC MCLK Enable Pin"},
-	{TAU_DAC_GPIO_MCLK_SELECT, GPIOF_OUT_INIT_LOW, "TauDAC MCLK Select Pin"},
-};
-*/
-
-
-/*
- * clocks
- */
-struct tau_dac_clks {
-	struct clk* ch;
-	char name[16];
+struct snd_soc_card_drvdata {
+	unsigned gpio_mclk_sel;
+	unsigned gpio_mclk_ena;
+	struct clk* lrclk_cpu;
+	struct clk* lrclk_dacl;
+	struct clk* lrclk_dacr;
+	struct clk* bclk_cpu;
+	struct clk* bclk_dacl;
+	struct clk* bclk_dacr;
 };
 
-static struct tau_dac_clks tau_dac_lrclks[] = {
-	{NULL, "lrclk-cpu"},
-	{NULL, "lrclk-dacl"},
-	{NULL, "lrclk-dacr"},
-};
-
-static struct tau_dac_clks tau_dac_bclks[] = {
-	{NULL, "bclk-cpu"},
-	{NULL, "bclk-dacl"},
-	{NULL, "bclk-dacr"},
-};
-
-static void tau_dac_enable_mclk(int index)
+static void tau_dac_enable_mclk(struct snd_soc_card_drvdata* drvdata,
+		unsigned int freq)
 {
-	gpio_set_value(gpio_mclk_sel, index);
-	gpio_set_value(gpio_mclk_ena, 1);
+	int index;
+
+	switch (freq) {
+	case 22579200: index = 0; break;
+	case 24576000: index = 1; break;
+	default: return;
+	}
+
+	gpio_set_value(drvdata->gpio_mclk_sel, index);
+	gpio_set_value(drvdata->gpio_mclk_ena, 1);
 	// TODO: msleep(20);
 }
 
-static void tau_dac_disable_mclk(void)
+static void tau_dac_disable_mclk(struct snd_soc_card_drvdata* drvdata)
 {
-	gpio_set_value(gpio_mclk_ena, 0);
+	gpio_set_value(drvdata->gpio_mclk_ena, 0);
 }
 
 /*
@@ -101,7 +89,11 @@ static int tau_dac_startup(struct snd_pcm_substream *substream)
 
 static void tau_dac_shutdown(struct snd_pcm_substream *substream)
 {
-	// TODO: disable mclk
+	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct snd_soc_card *soc_card = rtd->card;
+	struct snd_soc_card_drvdata *drvdata = snd_soc_card_get_drvdata(soc_card);
+
+	tau_dac_disable_mclk(drvdata);
 }
 
 static int tau_dac_hw_params(struct snd_pcm_substream *substream,
@@ -109,6 +101,8 @@ static int tau_dac_hw_params(struct snd_pcm_substream *substream,
 {
 	int ret, i;
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct snd_soc_card *soc_card = rtd->card;
+	struct snd_soc_card_drvdata *drvdata = snd_soc_card_get_drvdata(soc_card);
 	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
 	struct snd_soc_dai **codec_dais = rtd->codec_dais;
 	int num_codecs = rtd->num_codecs;
@@ -158,6 +152,7 @@ static int tau_dac_hw_params(struct snd_pcm_substream *substream,
 	}
 
 	// TODO: enable mclk, lrclk, bclk
+	tau_dac_enable_mclk(drvdata, mclk_freq);
 	return ret;
 }
 
@@ -187,7 +182,7 @@ static struct snd_soc_dai_link tau_dac_dai[] = {
 /*
  * asoc machine driver
  */
-static struct snd_soc_card tau_dac = {
+static struct snd_soc_card tau_dac_card = {
 	.name       = "tau_dac",
 	.dai_link   = tau_dac_dai,
 	.num_links  = ARRAY_SIZE(tau_dac_dai),
@@ -196,16 +191,13 @@ static struct snd_soc_card tau_dac = {
 /*
  * platform device driver
  */
-static int tau_dac_parse_dt(struct device_node *np)
+static int tau_dac_set_dai(struct device_node *np)
 {
 	int i;
 
 	struct device_node *i2s_node;
 	struct device_node *i2c_nodes[ARRAY_SIZE(tau_dac_codecs)];
 	struct snd_soc_dai_link *dai = &tau_dac_dai[0];
-	
-	int gpio, flags;
-	enum of_gpio_flags of_flags;
 
 	/* dais */
 	i2s_node = of_parse_phandle(np, "i2s-controller", 0);
@@ -227,116 +219,139 @@ static int tau_dac_parse_dt(struct device_node *np)
 		dai->codecs[i].name = NULL;
 		dai->codecs[i].of_node = i2c_nodes[i];
 	}
-	
-	/* gpios */
-	if (of_find_property(np, "gpio-mclk-sel", NULL)) {
-		gpio_mclk_sel = of_get_named_gpio_flags(np, "gpio-mclk-sel", 0, &of_flags);
-		if (!gpio_is_valid(gpio))
-			return -EINVAL;
 		
-		flags = (of_flags & OF_GPIO_ACTIVE_LOW) ? GPIOF_OUT_INIT_LOW :
-				GPIOF_OUT_INIT_HIGH;
-				
-		ret = devm_gpio_request_one(dev, gpio, flags,
-				dev->driver->name);
-		
-		// TODO: if (ret != 0) dev_err();
-	}
-
-
 //	if (snd_soc_of_parse_card_name(card, "atmel,model") != 0)
 //		dev_err(&pdev->dev, "snd_soc_of_parse_card_name() failed\n");
 
     return 0;
 }
 
-static int tau_dac_request_clocks(struct tau_dac_clks *clks, int n,
-		struct device *dev)
+static int tau_dac_get_gpio_from_of(struct device_node *np,
+		const char* name, unsigned* gpio, int* flags)
 {
-	int i;
-	
-	// DEBUG
-	return 0;
+	enum of_gpio_flags of_flags;
+	int of_gpio;
 
-	for (i = 0; i < n; i++) {
-		clks[i].ch = devm_clk_get(dev, clks[i].name);
-		if (IS_ERR(clks[i].ch))
+	if (of_find_property(np, name, NULL)) {
+		of_gpio = of_get_named_gpio_flags(np, name, 0, &of_flags);
+		if (!gpio_is_valid(of_gpio))
 			return -EINVAL;
+
+		*gpio = of_gpio;
+		*flags = (of_flags & OF_GPIO_ACTIVE_LOW) ? GPIOF_OUT_INIT_LOW :
+				GPIOF_OUT_INIT_HIGH;
+
+		return 0;
 	}
-	
+
+	return -EINVAL;
+}
+
+static int tau_dac_set_gpios(struct device *dev,
+		struct snd_soc_card_drvdata *drvdata)
+{
+	int ret = 0;
+	int flags;
+
+	ret |= tau_dac_get_gpio_from_of(dev->of_node, "gpio-mclk-sel",
+			&drvdata->gpio_mclk_sel, &flags);
+
+	ret |= devm_gpio_request_one(dev, drvdata->gpio_mclk_sel, flags,
+			"tau-dac_mclk-sel");
+
+	ret |= tau_dac_get_gpio_from_of(dev->of_node, "gpio-mclk-ena",
+				&drvdata->gpio_mclk_ena, &flags);
+
+	ret |= devm_gpio_request_one(dev, drvdata->gpio_mclk_ena, flags,
+			"tau-dac_mclk-ena");
+
+	return ret;
+}
+
+static int tau_dac_set_clocks(struct device *dev,
+		struct snd_soc_card_drvdata *drvdata)
+{
+	drvdata->lrclk_cpu = devm_clk_get(dev, "lrclk-cpu");
+	if (IS_ERR(drvdata->lrclk_cpu))
+		return -EINVAL;
+
+	drvdata->lrclk_dacl = devm_clk_get(dev, "lrclk-dacl");
+	if (IS_ERR(drvdata->lrclk_dacl))
+		return -EINVAL;
+
+	drvdata->lrclk_dacr = devm_clk_get(dev, "lrclk-dacr");
+	if (IS_ERR(drvdata->lrclk_dacr))
+		return -EINVAL;
+
+	drvdata->bclk_cpu = devm_clk_get(dev, "bclk-cpu");
+	if (IS_ERR(drvdata->bclk_cpu))
+		return -EINVAL;
+
+	drvdata->bclk_dacl = devm_clk_get(dev, "bclk-dacl");
+	if (IS_ERR(drvdata->bclk_dacl))
+		return -EINVAL;
+
+	drvdata->bclk_dacr = devm_clk_get(dev, "bclk-dacr");
+	if (IS_ERR(drvdata->bclk_dacr))
+		return -EINVAL;
+
 	return 0;
 }
 
 static int tau_dac_probe(struct platform_device *pdev)
 {
 	int ret;
-	struct device_node *np = pdev->dev.of_node;
+	struct device_node *np;
+	struct snd_soc_card_drvdata *drvdata;
 
-	tau_dac.dev = &pdev->dev;
+	tau_dac_card.dev = &pdev->dev;
 
-	/* parse device tree node info */
-	if (np != NULL) {
-		ret = tau_dac_parse_dt(np);
+	drvdata = devm_kzalloc(&pdev->dev, sizeof(*drvdata), GFP_KERNEL);
+	if (drvdata == NULL)
+		return -ENOMEM;
 
-		if (ret != 0) {
-			dev_err(&pdev->dev, "parsing device tree info failed: %d\n", ret);
-			goto err_dt;
-		}
-	} else {
-		dev_err(&pdev->dev, "only device tree supported\n");
-		return -EINVAL;
+	np = pdev->dev.of_node;
+	if (np == NULL) {
+		dev_err(&pdev->dev, "Device tree node not found\n");
+		return -ENODEV;
 	}
 
-	/* request gpio pins */
-//	ret = gpio_request_array(tau_dac_gpios, ARRAY_SIZE(tau_dac_gpios));
-/*
+	/* set dai */
+	ret = tau_dac_set_dai(np);
 	if (ret != 0) {
-		dev_err(&pdev->dev, "gpio_request_array() failed: %d\n", ret);
-		goto err_gpio;
+		dev_err(&pdev->dev, "Setting dai failed: %d\n", ret);
+		return ret;
 	}
-*/
-	
-	/* get clocks */
-	ret = tau_dac_request_clocks(tau_dac_lrclks,
-			ARRAY_SIZE(tau_dac_lrclks), &pdev->dev);
-	
-	if (ret != 0) {
-		dev_err(&pdev->dev, "getting frame clocks failed: %d\n", ret);
-		goto err_clk;
-	}
-	
-	ret = tau_dac_request_clocks(tau_dac_bclks,
-			ARRAY_SIZE(tau_dac_bclks), &pdev->dev);
 
+	/* set gpios */
+	ret = tau_dac_set_gpios(&pdev->dev, drvdata);
 	if (ret != 0) {
-		dev_err(&pdev->dev, "getting bit clocks failed: %d\n", ret);
-		goto err_clk;
+		dev_err(&pdev->dev, "Setting gpios failed: %d\n", ret);
+		return ret;
+	}
+
+	/* set clocks */
+	ret = tau_dac_set_clocks(&pdev->dev, drvdata);
+	if (ret != 0) {
+		dev_err(&pdev->dev, "Setting clocks failed: %d\n", ret);
+		return ret;
 	}
 	
 	/* register card */
-	ret = snd_soc_register_card(&tau_dac);
+	snd_soc_card_set_drvdata(&tau_dac_card, drvdata);
+	ret = snd_soc_register_card(&tau_dac_card);
 
 	if (ret != 0) {
 		dev_err(&pdev->dev, "snd_soc_register_card() failed: %d\n", ret);
-		goto err_gpio;
+		return ret;
 	}
 
-	return ret;
-	
-err_clk:
-//err_gpio:
-//	gpio_free_array(tau_dac_gpios, ARRAY_SIZE(tau_dac_gpios));
-err_dt:
 	return ret;
 }
 
 static int tau_dac_remove(struct platform_device *pdev)
 {
-	gpio_set_value(TAU_DAC_GPIO_MCLK_ENABLE, 0);
-	gpio_set_value(TAU_DAC_GPIO_MCLK_SELECT, 0);
-	gpio_free_array(tau_dac_gpios, ARRAY_SIZE(tau_dac_gpios));
-
-	return snd_soc_unregister_card(&tau_dac);
+	return snd_soc_unregister_card(&tau_dac_card);
 }
 
 static const struct of_device_id tau_dac_of_match[] = {
